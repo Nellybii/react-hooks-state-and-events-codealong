011a993166bd022301291bf73414a810ea4c0c5d
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-10-24 18:06:30 +0300

    background color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
-
+//import useState hook
+import React, { useState } from "react";
 function Toggle() {
-  return <button>OFF</button>;
+  // initiate state
+  const [isOn, setIsOn] = useState(false);
+  // create a setter function
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+  // use state variable in the component
+  return (
+    <button onClick={handleClick} style={{ background: "red" }}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
